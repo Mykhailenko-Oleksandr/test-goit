2f19602431bec0b3e782a8df95999ac403a4695f
Sapper <saniamykhailenko@gmail.com>
2026-03-31 09:53:04 +0300

    init repo

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,14 +1,14 @@
-/** 
+/**
  * У файлі constants.js зберігай константи (end-points), які будуть використовуватись в роботі.
  */
 
-export const API_BASE_URL = "https://furniture-store.b.goit.study/api";
+export const API_BASE_URL = 'https://example.com/register';
 
-export const API_ENDPOINTS = {
-    FURNITURES: "/furnitures",
-    CATEGORIES: "/categories",
-    ORDERS: "/orders",
-    FEEDBACKS: "/feedbacks",
-}
+// export const API_ENDPOINTS = {
+//   FURNITURES: '/furnitures',
+//   CATEGORIES: '/categories',
+//   ORDERS: '/orders',
+//   FEEDBACKS: '/feedbacks',
+// };
 
-export const ITEMS_PER_PAGE = 8;
+// export const ITEMS_PER_PAGE = 8;
