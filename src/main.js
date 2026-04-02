cd76d49831033c3a3e42e5d99e29ec1bcbb764c5
Sapper <saniamykhailenko@gmail.com>
2026-04-02 15:19:16 +0300

    delete comments

--- src/main.js
+++ src/main.js
@@ -1,11 +1,5 @@
 import { handleCheckWidth } from './js/handlers';
 import { startTimer } from './js/timer';
-import { refs } from './js/refs';
-
-/**
- * У файлі main.js логіка сторінки Index (index.html)
- */
-// console.log(refs.JoinInputName);
 
 window.addEventListener('resize', handleCheckWidth);
 handleCheckWidth();
